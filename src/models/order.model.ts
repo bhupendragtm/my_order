981114e16a6484968ab6c2592f506dc085e8112f
Bhupendra Gautam <bhupendragtm777@gmail.com>
2022-08-01 20:02:29 +0000

    First Commit1

---
+++ src/models/order.model.ts
@@ -0,0 +1,48 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Customer} from './customer.model';
+
+@model({settings: {strict: false}})
+export class Order extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  date: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  person: string;
+
+  @belongsTo(() => Customer)
+  customerId: string;
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Order>) {
+    super(data);
+  }
+}
+
+export interface OrderRelations {
+  // describe navigational properties here
+}
+
+export type OrderWithRelations = Order & OrderRelations;
---
+++ src/repositories/order.repository.ts
@@ -0,0 +1,22 @@
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
+import {DbDataSource} from '../datasources';
+import {Order, OrderRelations, Customer} from '../models';
+import {CustomerRepository} from './customer.repository';
+
+export class OrderRepository extends DefaultCrudRepository<
+  Order,
+  typeof Order.prototype.id,
+  OrderRelations
+> {
+
+  public readonly customer: BelongsToAccessor<Customer, typeof Order.prototype.id>;
+
+  constructor(
+    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('CustomerRepository') protected customerRepositoryGetter: Getter<CustomerRepository>,
+  ) {
+    super(Order, dataSource);
+    this.customer = this.createBelongsToAccessorFor('customer', customerRepositoryGetter,);
+    this.registerInclusionResolver('customer', this.customer.inclusionResolver);
+  }
+}
